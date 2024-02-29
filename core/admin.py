727eafff7b43257e61fbd78793b7e0a40b2697be
alex <tulyulyuk91@gmail.com>
2024-02-29 10:30:14 +0100

    add django project with models and initial migration

---
+++ core/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import TaskType, Task, Worker, Position
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(TaskType)
+admin.site.register(Task)
+admin.site.register(Position)
---
+++ core/models.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    priorities = [
+        ("URG", "Urgent"),
+        ("HIG", "High"),
+        ("MED", "Medium"),
+        ("LOW", "Low")
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(blank=True, default=False)
+    priority = models.CharField(max_length=255, choices=priorities)
+    task_type = models.ForeignKey(to=TaskType, on_delete=models.SET_NULL, null=True, related_name="tasks")
+    assignees = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name="tasks")
+
+    class Meta:
+        ordering = ("deadline",)
+
+    def __str__(self):
+        return f"{self.name} ({self.priority} / finish before {self.deadline})"
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(to=Position, on_delete=models.SET_NULL, null=True, blank=True)
